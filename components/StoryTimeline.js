c2ea02051218558e85d960c878279968d4ea1b58
Atishay Kasliwal <61780293+atishay-kasliwal@users.noreply.github.com>
2025-11-18 22:23:31 -0500

    Fix: Move all remaining images to public folder and add Header/Footer to layout

--- components/StoryTimeline.js
+++ components/StoryTimeline.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import img1 from '../src/assets/Symbiosis-Indore-Auditorium-Front-View.jpg';
+// Image moved to public folder - use direct path
+const img1 = '/Symbiosis-Indore-Auditorium-Front-View.jpg';
 
 const stories = [
   {
